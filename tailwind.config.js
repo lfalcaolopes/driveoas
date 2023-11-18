2977a44be592d9a591b26f25c42f6ca2ad40cdc9
lfalcaolopes <lfalcaolopes@gmail.com>
2023-11-18 02:45:06 -0300

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  // purge: [
+  //   './src/components/**/*.{js,ts,jsx,tsx}',
+  //   './src/components/*.{js,ts,jsx,tsx}',
+  //   './src/pages/*.{js,ts,jsx,tsx}',
+  //   './src/*.{js,ts,jsx,tsx}',
+  // ],
   theme: {
     extend: {
       flexGrow: {
